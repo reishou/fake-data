6f857061abf1df622d7ac3fa5efea016bbce9ded
Reishou <reishou90@gmail.com>
2021-03-10 17:25:14 +0700

    add go module

--- main.go
+++ main.go
@@ -11,22 +11,22 @@ import (
 )
 
 type User struct {
-	name     string
-	email    string
-	password string
-	birthday time.Time
-	phone    string
-	address  string
+	Name     string `faker:"name"`
+	Email    string `faker:"email"`
+	Password string `faker:"password"`
+	Birthday string `faker:"date"`
+	Phone    string `faker:"e_164_phone_number"`
+	Address  string `faker:"mac_address"`
 }
 
 func (u User) toString() []string {
 	return []string{
-		u.name,
-		u.email,
-		u.password,
-		u.birthday.Format("Y-m-d"),
-		u.phone,
-		u.address,
+		u.Name,
+		u.Email,
+		u.Password,
+		u.Birthday,
+		u.Phone,
+		u.Address,
 	}
 }
 
@@ -36,7 +36,7 @@ func logTime() {
 }
 
 func touchCsv(name string) (*csv.Writer, *os.File) {
-	file, err := os.Create("csv/%s.csv")
+	file, err := os.Create("csv/" + name + ".csv")
 
 	if err != nil {
 		log.Fatalf("Fail creating file: %s", err)
@@ -49,7 +49,7 @@ func writeCsv(writer *csv.Writer, file *os.File, n int64) {
 	var i int64
 	for i < n {
 		i += 1
-		var user User
+		user := User{}
 		_ = faker.FakeData(&user)
 		_ = writer.Write(user.toString())
 	}
@@ -59,9 +59,19 @@ func writeCsv(writer *csv.Writer, file *os.File, n int64) {
 }
 
 func main() {
-	writer, file := touchCsv("data")
-	logTime()
-	n := math.Pow(10, 7)
-	writeCsv(writer, file, int64(n))
-	logTime()
+	args := os.Args
+
+	for index, arg :=range args{
+		if index > 0  {
+			if file, err := os.Stat("template/" + arg + ".yml"); err == nil {
+				fmt.Print(file)
+			}
+		}
+	}
+	//file, _ = os.ReadFile("template/")
+	//writer, file := touchCsv("data")
+	//logTime()
+	//n := math.Pow(10, 7)
+	//writeCsv(writer, file, int64(n))
+	//logTime()
 }
