04068e23cefd04d53ea4745cb93151979e621806
Reishou <reishou90@gmail.com>
2021-03-11 17:06:08 +0700

    get schema yml and write csv

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/reishou/utils
 
 go 1.16
 
-require github.com/bxcodec/faker/v3 v3.6.0
+require (
+	github.com/bxcodec/faker/v3 v3.6.0
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
--- main.go
+++ main.go
@@ -4,39 +4,25 @@ import (
 	"encoding/csv"
 	"fmt"
 	"github.com/bxcodec/faker/v3"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 	"math"
 	"os"
 	"time"
 )
 
-type User struct {
-	Name     string `faker:"name"`
-	Email    string `faker:"email"`
-	Password string `faker:"password"`
-	Birthday string `faker:"date"`
-	Phone    string `faker:"e_164_phone_number"`
-	Address  string `faker:"mac_address"`
-}
-
-func (u User) toString() []string {
-	return []string{
-		u.Name,
-		u.Email,
-		u.Password,
-		u.Birthday,
-		u.Phone,
-		u.Address,
-	}
+type Schema struct {
+	Schema []string
 }
 
 func logTime() {
 	now := time.Now()
-	fmt.Printf("%d\n", now.Unix())
+	fmt.Println(now.Unix())
 }
 
 func touchCsv(name string) (*csv.Writer, *os.File) {
-	file, err := os.Create("csv/" + name + ".csv")
+	file, err := os.Create(fmt.Sprintf("csv/%s.csv", name))
 
 	if err != nil {
 		log.Fatalf("Fail creating file: %s", err)
@@ -45,33 +31,71 @@ func touchCsv(name string) (*csv.Writer, *os.File) {
 	return csv.NewWriter(file), file
 }
 
-func writeCsv(writer *csv.Writer, file *os.File, n int64) {
+func writeCsv(writer *csv.Writer, file *os.File, n int64, schema Schema) {
 	var i int64
 	for i < n {
 		i += 1
-		user := User{}
-		_ = faker.FakeData(&user)
-		_ = writer.Write(user.toString())
+		_ = writer.Write(makeData(schema))
 	}
 
 	writer.Flush()
 	_ = file.Close()
 }
 
-func main() {
-	args := os.Args
+func makeData(schema Schema) []string {
+	var data []string
 
-	for index, arg :=range args{
-		if index > 0  {
-			if file, err := os.Stat("template/" + arg + ".yml"); err == nil {
-				fmt.Print(file)
-			}
+	for _, v := range schema.Schema {
+		value := callFuncByName(v)
+		data = append(data, value)
+	}
+
+	return data
+}
+
+func callFuncByName(name string) string {
+	if name == "lat" {
+		return fmt.Sprintf("%f", faker.Latitude())
+	} else if name == "long"{
+		return fmt.Sprintf("%f", faker.Longitude())
+	} else if name == "cc_number"{
+		return faker.CCNumber()
+	} else if name == "cc_type"{
+		return faker.CCType()
+	}
+	return faker.Sentence()
+}
+
+func exportCsv(name string, schema Schema) {
+	writer, file := touchCsv(name)
+	logTime()
+	n := 2 * math.Pow(10, 6)
+	writeCsv(writer, file, int64(n), schema)
+	logTime()
+}
+
+func main() {
+	implementArgs(func(arg string) {
+		schema := getSchema(arg)
+		exportCsv(arg, schema)
+	})
+}
+
+func getSchema(arg string) Schema {
+	source, err := ioutil.ReadFile("template/" + arg + ".yml")
+	if err != nil {
+		panic(err)
+	}
+	var schema Schema
+	_ = yaml.Unmarshal(source, &schema)
+	return schema
+}
+
+func implementArgs(f func(arg string)) {
+	args := os.Args
+	for index, arg := range args {
+		if index > 0 {
+			f(arg)
 		}
 	}
-	//file, _ = os.ReadFile("template/")
-	//writer, file := touchCsv("data")
-	//logTime()
-	//n := math.Pow(10, 7)
-	//writeCsv(writer, file, int64(n))
-	//logTime()
 }
